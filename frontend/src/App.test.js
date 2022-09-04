d48c5525839343136864d20c3629612b3b8ca9c1
craig-glass <sydrther@gmail.com>
2022-09-04 15:01:55 +0100

    Added test in tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,4 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+it('should take a snapshot', () => {
+    const { asFragment } = render(<App />)
 
+    expect(asFragment(<App />)).toMatchSnapshot()
+}) 
