7a0e12a5a8239d6ad61f40f9f5714099e97a094c
craig-glass <sydrther@gmail.com>
2022-09-15 10:06:53 +0100

    Added framer motion

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,28 +1,29 @@
 import React from "react";
-import Alien from '../images/Aliens.jpg';
+import Alien from "../images/Aliens.jpg";
 
-export default function About(){
-    return (
-        <section id="about">
-            <div>
-                <div>
-                    <h1 style={{ color: "lightgreen"}}>
-                        Hi, I'm Craig.
-                        <br></br>
-                    </h1>
-                    <p className="squashSides">
-                    I am a competent developer with experience implementing both the backend and 
-                    frontend of applications. I understand all aspects of the software development 
-                    lifecycle and I am comfortable working with both agile and waterfall methodologies. 
-                    I am seeking to find a position in a company in which I can expand my knowledge and 
-                    grow my skills.
-                    </p>
-                </div>
-                <div>
-                    <br></br>
-                    <img alt="hero" src={Alien} width={200} height={200}></img>
-                </div>
-            </div>
-        </section>
-    )
+export default function About() {
+  return (
+    <section id="about">
+      <div>
+        <div>
+          <h1 style={{ color: "#328cc1" }}>
+            Hi, I'm Craig.
+            <br></br>
+          </h1>
+          <p className="squashSides">
+            I am a competent developer with experience implementing both the
+            backend and frontend of applications. I understand all aspects of
+            the software development lifecycle and I am comfortable working with
+            both agile and waterfall methodologies. I am seeking to find a
+            position in a company in which I can expand my knowledge and grow my
+            skills.
+          </p>
+        </div>
+        <div>
+          <br></br>
+          <img alt="hero" src={Alien} width={200} height={200}></img>
+        </div>
+      </div>
+    </section>
+  );
 }
