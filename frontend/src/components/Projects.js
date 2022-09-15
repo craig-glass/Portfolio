d03e107d887f5088d29fef446d6e39399bea2710
craig-glass <sydrther@gmail.com>
2022-09-15 10:21:15 +0100

    Added animation to projet items scale with bounce

--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -1,24 +1,35 @@
 import { projects } from "../data/projectsData";
+import { motion } from "framer-motion";
 
 export default function Projects() {
-    return (
-        <div>
-            <h1>Projects</h1>
-            <br></br>
-            <div className="grid-container">
-                {projects.map((project) => (
-                    <>
-                        <div className="grid-item">
-                            <h2>{project.title}</h2>
-                            <img alt="hero" src={project.image} width="100%" height="25%"></img>
-                            <h4>{project.subtitle}</h4>
-                            <p>{project.description}</p>
-                            <a href={project.link}>Click here to see project</a>
-                        </div>
-                    </>
-                ))}
-            </div>
-
-        </div>
-    )
+  return (
+    <div>
+      <h1>Projects</h1>
+      <br></br>
+      <div className="grid-container">
+        {projects.map((project) => (
+          <>
+            <motion.div
+              className="grid-item"
+              whileHover={{
+                scale: 1.2,
+                transition: { type: "spring", bounce: 0.8 },
+              }}
+            >
+              <h2>{project.title}</h2>
+              <img
+                alt="hero"
+                src={project.image}
+                width="100%"
+                height="25%"
+              ></img>
+              <h4>{project.subtitle}</h4>
+              <p>{project.description}</p>
+              <a href={project.link}>Click here to see project</a>
+            </motion.div>
+          </>
+        ))}
+      </div>
+    </div>
+  );
 }
