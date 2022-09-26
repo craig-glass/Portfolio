2060f6c7868c091cf16dc6742505231e187dc660
craig-glass <sydrther@gmail.com>
2022-09-26 12:04:10 +0100

    Added social media icons to sidebar

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -5,6 +5,9 @@ import { useState } from "react";
 import { GiSkills } from "react-icons/gi";
 import { AiOutlineMail } from "react-icons/ai";
 import { FaProjectDiagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 export default function Navbar() {
   const variants = {
@@ -35,6 +38,18 @@ export default function Navbar() {
           <Link to="/projects" onClick={showSidebar}>
             <FaProjectDiagram style={{ width: 30, heigth: 30 }} />
           </Link>
+
+          <div className="socialIcons">
+            <a href="https://github.com/craig-glass">
+              <FaGithub />
+            </a>
+            <a href="https://twitter.com/CraigGl05802336">
+              <FaTwitter />
+            </a>
+            <a href="https://www.linkedin.com/in/craig-glass-2b142424b/">
+              <FaLinkedin />
+            </a>
+          </div>
         </motion.header>
       </div>
 
